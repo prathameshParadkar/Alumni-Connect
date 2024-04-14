0178dddd1c72753345cd828074bf69892a30a9f5
MuursaleenBats <mursaleenbatatawala@gmail.com>
2024-04-14 17:20:48 +0530

    database connection

--- server/server/server.js
+++ server/server/server.js
@@ -1,28 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const mongoose = require('mongoose');
-
-const { getSecret } = require('./secrets');
-const usersRoute = require('./routes/users');
-
-mongoose.Promise = global.Promise;
-mongoose.connect(getSecret('dbUri')).then(
-  () => {
-    console.log('Connected to mongoDB');
-  },
-  (err) => console.log('Error connecting to mongoDB', err)
-);
+const connectDB = require('./utils/db');
 
 const app = express();
-const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use('/api/users', usersRoute);
+app.use(express.json());
+connectDB();
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
-
-module.exports = { app };
